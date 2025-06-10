33bf2e0a98e940c5c7cceebd476e5503000b1740
ParthaPraneeth01 <parthapraneeth01@gmail.com>
2025-06-10 14:30:08 +0530

    Create SelectionSort.java

---
+++ Sortings/SelectionSort.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class SelectionSort
+{
+	public static void main(String[] args) {
+	    int [] arr = new int[5];
+	    Scanner sc= new Scanner(System.in);
+	    for(int i=0;i<5;i++){
+	        arr[i]=sc.nextInt();
+	    }
+	    
+	    for(int i=0;i<arr.length-1;i++){
+	        int mini=i;
+	        for(int j=i;j<arr.length;j++){
+	            if(arr[j]<arr[mini]){
+	                mini=j;
+	            }
+	        }
+	        int temp = arr[i];
+	        arr[i]=arr[mini];
+	        arr[mini]=temp;
+	    }
+	    
+	    for(int i=0;i<5;i++){
+	        System.out.println(arr[i]);
+	    }
+	}
+}
