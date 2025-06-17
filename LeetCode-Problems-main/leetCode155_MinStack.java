ae58a97f9b00f29a80ed7fbf48e5a62ce847ff98
ParthaPraneeth01 <parthapraneeth01@gmail.com>
2025-06-17 13:47:46 +0530

    Add files via upload

---
+++ LeetCode-Problems-main/leetCode155_MinStack.java
@@ -0,0 +1,39 @@
+class MinStack {
+    Stack<Integer> mainStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if(minStack.isEmpty() || val<=minStack.peek()){
+            minStack.push(val);
+        }
+        else{
+            minStack.push(minStack.peek());
+        }
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
